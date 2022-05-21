a05f2b2ca25bdea5f464658f782e9ac6a37b8536
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2022-05-21 20:27:27 -0300

    Add current contracts in index.js

--- blockchain/chaincode/index.js
+++ blockchain/chaincode/index.js
@@ -2,10 +2,12 @@
 
 const Course = require('./lib/course');
 const InstanceType = require('./lib/instance_type');
-const Instance = require('./lib/instance');
+const Professor = require('./lib/professor');
+const Student = require('./lib/student');
 
 module.exports.Course = Course;
 module.exports.InstanceType = InstanceType;
-module.exports.Instance = Instance;
+module.exports.Professor = Professor;
+module.exports.Student = Student;
 
-module.exports.contracts = [ Course, InstanceType, Instance ];
+module.exports.contracts = [ Course, InstanceType, Professor, Student ];
