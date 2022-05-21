9857d3055a618b4fa8b793cc8d64632ea996069a
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2022-05-21 20:41:37 -0300

    Add instance and note in index.js

--- blockchain/chaincode/index.js
+++ blockchain/chaincode/index.js
@@ -4,10 +4,14 @@ const Course = require('./lib/course');
 const InstanceType = require('./lib/instance_type');
 const Professor = require('./lib/professor');
 const Student = require('./lib/student');
+const Instance = require('./lib/instance');
+const Note = require('./lib/note');
 
 module.exports.Course = Course;
 module.exports.InstanceType = InstanceType;
 module.exports.Professor = Professor;
 module.exports.Student = Student;
+module.exports.Instance = Instance;
+module.exports.Note = Note;
 
-module.exports.contracts = [ Course, InstanceType, Professor, Student ];
+module.exports.contracts = [ Course, InstanceType, Professor, Student, Instance, Note ];
